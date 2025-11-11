8a046e7ca092e1faaa0280b9139d9ae14cc7cf7c
Nikashi33 <naufalblue33@gmail.com>
2025-11-11 20:14:38 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const config = {
         serverLogoImageFileName: "logo.png", /*This is a file name for logo in /images/ (If you upload new logo with other name, you must change this value)*/
         serverName: "Rutinitas Pemain Roblox", /*Server name*/
         serverIp: "slime.skyes.id:7026", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
-        discordServerID: "1278672938064941126" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
+        discordServerID: "1224228387032137768" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
     },
 
     /*Admin-Team
